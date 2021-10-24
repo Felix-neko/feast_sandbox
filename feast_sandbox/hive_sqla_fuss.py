49ff5fb8bf168a15cc04e2a6a6744a5af5c82bc1
Felix-Neko <felix-neko@list.ru>
2021-10-24 11:56:31 +0300

    Regenerated feast online store. Now somehow works...

--- feast_sandbox/hive_sqla_fuss.py
+++ feast_sandbox/hive_sqla_fuss.py
@@ -28,5 +28,3 @@ if __name__ == "__main__":
     print(stmt)
 
     print(conn.execute(stmt).all())
-
-ubun
--- feast_sandbox/main.py
+++ feast_sandbox/main.py
@@ -1,18 +1,42 @@
 from pprint import pprint
 from feast import FeatureStore
+import pandas as pd
+from datetime import datetime, timedelta
 
 if __name__ == "__main__":
+    # The entity dataframe is the dataframe we want to enrich with feature values
+
+    fs = FeatureStore("../feature_repo")
+
+    entity_df = pd.DataFrame.from_dict(
+        {
+            "driver_id": [1001, 1002, 1003],
+            "label_driver_reported_satisfaction": [1, 5, 3],
+            "event_timestamp": [
+                datetime.now() - timedelta(minutes=11),
+                datetime.now() - timedelta(minutes=36),
+                datetime.now() - timedelta(minutes=73),
+            ],
+        }
+    )
+
     store = FeatureStore(repo_path="../feature_repo")
-    feature_vector = store.get_online_features(
+
+    training_df = store.get_historical_features(
+        entity_df=entity_df,
         features=[
             "driver_hourly_stats:conv_rate",
             "driver_hourly_stats:acc_rate",
             "driver_hourly_stats:avg_daily_trips",
         ],
-        entity_rows=[
-            {"driver_id": 1004},
-            {"driver_id": 1005},
-        ],
-    ).to_dict()
+    ).to_df()
 
-    pprint(feature_vector)
+    print("----- Feature schema -----\n")
+    print(training_df.info())
+
+    print()
+    print("----- Example features -----\n")
+    print(training_df.head())
+
+    pass
+
