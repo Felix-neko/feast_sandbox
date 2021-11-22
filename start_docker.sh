c50c2728662d22fe73d685dd8cfcceb35b85a43e
Felix <felix-neko@list.ru>
2021-11-22 18:24:49 +0300

    Minor fix.

--- start_docker.sh
+++ start_docker.sh
@@ -3,4 +3,4 @@ cd $BASEDIR/docker_data
 
 docker-compose stop
 docker-compose -f ../docker-hive/docker-compose.yml -f ./docker-compose.extras.yml config > ./docker-compose.yml
-docker-compose up -d
+CURRENT_UID=$(id -u):$(id -g) docker-compose up -d
