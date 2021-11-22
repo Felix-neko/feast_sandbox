ec0c4709aa5f0ec76ca8d79ec313a9aa91d2a909
Felix <felix-neko@list.ru>
2021-11-22 18:24:49 +0300

    Minor fix.

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,5 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR/docker_data
-
-docker-compose stop
 docker-compose -f ../docker-hive/docker-compose.yml -f ./docker-compose.extras.yml config > ./docker-compose.yml
-docker-compose up -d
+docker-compose stop
+CURRENT_UID=$(id -u):$(id -g) docker-compose up -d
