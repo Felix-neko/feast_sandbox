b2d5d7c3f979d7aff1a4a381cb091721d47e865c
Nik Brezginov <nbrezginov@alfabank.ru>
2022-01-28 15:48:19 +0300

    fix cp hadoop-hive.env to docker_data

--- start_docker.sh
+++ start_docker.sh
@@ -1,5 +1,6 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR/docker_data
+yes | cp -rf ../docker-hive/hadoop-hive.env ../docker_data/hadoop-hive.env
 docker-compose -f ../docker-hive/docker-compose.yml -f ./docker-compose.extras.yml config > ./docker-compose.yml
 docker-compose stop
 CURRENT_UID=$(id -u):$(id -g) docker-compose up -d
